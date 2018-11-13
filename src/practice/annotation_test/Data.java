455b2f737501a9d33cb3bdecb7c8f4e444ffcd01
naget <cloved9999@gmail.com>
2018-11-13 10:46:26 +0800

    利用注解在运行期自定义行为

--- src/practice/annotation_test/Data.java
+++ src/practice/annotation_test/Data.java
@@ -8,8 +8,9 @@ import java.lang.annotation.Target;
 /**
  * created by tianfeng on 2018/11/12
  */
-@Retention(RetentionPolicy.CLASS)
+@Retention(RetentionPolicy.RUNTIME)//如果改成.CLASS，注解在运行期机会被丢弃
 @Target(ElementType.TYPE)
 public @interface Data {
-    
+    public String id();
+    public String description() default "no description";
 }
--- src/practice/annotation_test/User.java
+++ src/practice/annotation_test/User.java
@@ -3,7 +3,19 @@ package practice.annotation_test;
 /**
  * created by tianfeng on 2018/11/12
  */
+@Data(id = "1")
 public class User {
     private String name;
     private String passward;
+    public User(){}
+
+    public static void main(String[] args) {
+        User.annotationProcesser(User.class);
+    }
+    public static void annotationProcesser(Class<?> c){
+        Data data = c.getAnnotation(Data.class);//运行期被丢弃的话这儿就拿不到了
+        if (data!=null){
+            System.out.println(data.id()+data.description());
+        }
+    }
 }
